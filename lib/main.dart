916cb6f2fa93511b342654d3b1b492fd6bb14cd1
Priggelpitt <prgglptt@gmail.com>
2019-09-24 14:21:58 +0200

    Imported the dummy-screens to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nesteo_app/bloc/bloc.dart';
+import 'package:nesteo_app/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
