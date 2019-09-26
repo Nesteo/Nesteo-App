7f0b8f022e21e90ea11a77730d7e2d0e6642dcb8
Priggelpitt <prgglptt@gmail.com>
2019-09-26 16:53:24 +0200

    Replaced the placeholder-login with a more advanced version

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,20 +5,127 @@ import 'package:nesteo_app/blocs/pagecontrol_bloc/pagecontrol.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final PageControlBloc pageControlBloc =
-        BlocProvider.of<PageControlBloc>(context);
+    return LoginPage();
+  }
+}
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Screen'),
+      backgroundColor: Colors.lightGreen,
+      body: LoginContent(),
+    );
+  }
+}
+
+class LoginContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: SplashScreen(),
+            padding: EdgeInsets.only(bottom: 40),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: 300,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Domain',
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.location_city, color: Colors.green),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: 300,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.person, color: Colors.green),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            width: 300,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(Icons.lock, color: Colors.green),
+                ),
+                obscureText: true,
+              ),
+            ),
+          ),
+          Container(
+            width: 250,
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: RaisedButton(
+                color: Colors.green,
+                onPressed: () {
+                  BlocProvider.of<PageControlBloc>(context)
+                      .dispatch(GoToHomeEvent());
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            pageControlBloc.dispatch(GoToHomeEvent());
-          },
-        ),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.location_on,
+            size: 100,
+            color: Colors.green,
+          ),
+          Text(
+            'Nesteo',
+            style: TextStyle(
+              fontFamily: 'Quicksand',
+              fontSize: 48,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
