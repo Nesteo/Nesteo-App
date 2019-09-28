59deb878daab9cb83adafd841475acffa79c7e44
Priggelpitt <prgglptt@gmail.com>
2019-09-28 17:18:46 +0200

    Renamed InfoBox to BoxInfo

--- lib/blocs/pagecontrol_bloc/pagecontrol_bloc.dart
+++ lib/blocs/pagecontrol_bloc/pagecontrol_bloc.dart
@@ -10,63 +10,57 @@ class PageControlBloc extends Bloc<PageControlEvent, PageControlState> {
   Stream<PageControlState> mapEventToState(
     PageControlEvent event,
   ) async* {
-    if (event is GoToHomeEvent) {
-      yield ToHomeScreenState();
-
-      // TODO: This is just a way to "simulate" a delay for now!
-      await Future.delayed(Duration(seconds: 1));
-      yield HomeScreenState();
-    }
-    if (event is HomeEvent) {
-      yield HomeScreenState();
-    }
     if (event is GoToMapEvent) {
       yield ToMapScreenState();
 
-      // TODO: This is just a way to "simulate" a delay for now!
       await Future.delayed(Duration(seconds: 1));
       yield MapScreenState();
     }
+
     if (event is MapEvent) {
       yield MapScreenState();
     }
+
     if (event is GoToBoxListEvent) {
       yield ToBoxListScreenState();
 
-      // TODO: This is just a way to "simulate" a delay for now!
       await Future.delayed(Duration(seconds: 1));
       yield BoxListScreenState();
     }
+
     if (event is BoxListEvent) {
       yield BoxListScreenState();
     }
+
     if (event is GoToNewBoxEvent) {
       yield ToNewBoxScreenState();
 
-      // TODO: This is just a way to "simulate" a delay for now!
       await Future.delayed(Duration(seconds: 1));
       yield NewBoxScreenState();
     }
+
     if (event is NewBoxEvent) {
       yield NewBoxScreenState();
     }
-    if (event is GoToInfoBoxEvent) {
-      yield ToInfoBoxScreenState();
 
-      // TODO: This is just a way to "simulate" a delay for now!
+    if (event is GoToBoxInfoEvent) {
+      yield ToBoxInfoScreenState();
+
       await Future.delayed(Duration(seconds: 1));
-      yield InfoBoxScreenState();
+      yield BoxInfoScreenState();
     }
-    if (event is InfoBoxEvent) {
-      yield InfoBoxScreenState();
+
+    if (event is BoxInfoEvent) {
+      yield BoxInfoScreenState();
     }
+
     if (event is GoToInspectionEvent) {
       yield ToInspectionScreenState();
 
-      // TODO: This is just a way to "simulate" a delayfor now!
       await Future.delayed(Duration(seconds: 1));
       yield InspectionScreenState();
     }
+
     if (event is InspectionEvent) {
       yield InspectionScreenState();
     }
