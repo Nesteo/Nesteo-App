305ea65a2877acb83875761c2c25f13e2f176bba
Priggelpitt <prgglptt@gmail.com>
2019-10-08 16:04:30 +0200

    Updated blocs due to new Equatable version

--- lib/blocs/pagecontrol_bloc/pagecontrol_event.dart
+++ lib/blocs/pagecontrol_bloc/pagecontrol_event.dart
@@ -3,33 +3,75 @@ import 'package:meta/meta.dart';
 
 @immutable
 abstract class PageControlEvent extends Equatable {
-  PageControlEvent([List props = const <dynamic>[]]) : super(props);
+  PageControlEvent([List props = const <dynamic>[]]);
 }
 
-class GoToHomeEvent extends PageControlEvent {}
+class GoToHomeEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class HomeEvent extends PageControlEvent {}
+class HomeEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToMapEvent extends PageControlEvent {}
+class GoToMapEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class MapEvent extends PageControlEvent {}
+class MapEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToBoxListEvent extends PageControlEvent {}
+class GoToBoxListEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class BoxListEvent extends PageControlEvent {}
+class BoxListEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToNewBoxEvent extends PageControlEvent {}
+class GoToNewBoxEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class NewBoxEvent extends PageControlEvent {}
+class NewBoxEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToBoxInfoEvent extends PageControlEvent {}
+class GoToBoxInfoEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class BoxInfoEvent extends PageControlEvent {}
+class BoxInfoEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToInspectionEvent extends PageControlEvent {}
+class GoToInspectionEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class InspectionEvent extends PageControlEvent {}
+class InspectionEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class GoToNewInspectionEvent extends PageControlEvent {}
+class GoToNewInspectionEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
 
-class NewInspectionEvent extends PageControlEvent {}
+class NewInspectionEvent extends PageControlEvent {
+  @override
+  List<Object> get props => [];
+}
