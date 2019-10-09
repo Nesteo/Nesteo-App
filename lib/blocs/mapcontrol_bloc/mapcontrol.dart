fffa20e15d1a77197e4c22c91177605889d0fb36
Priggelpitt <prgglptt@gmail.com>
2019-10-09 16:37:20 +0200

    Added new MapControlBloc. Resolves #40

---
+++ lib/blocs/mapcontrol_bloc/mapcontrol.dart
@@ -0,0 +1,3 @@
+export 'mapcontrol_bloc.dart';
+export 'mapcontrol_event.dart';
+export 'mapcontrol_state.dart';
---
+++ lib/blocs/mapcontrol_bloc/mapcontrol_bloc.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import './mapcontrol.dart';
+
+class MapControlBloc extends Bloc<MapControlEvent, MapControlState> {
+  GoogleMapController controller;
+  GoogleMap map;
+  LatLng location = LatLng(52.3537269, 9.724127);
+  MapType mapType = MapType.normal;
+  double zoom = 16;
+  double tilt = 20;
+
+  @override
+  MapControlState get initialState => InitialMapControlState();
+
+  @override
+  Stream<MapControlState> mapEventToState(
+    MapControlEvent event,
+  ) async* {
+    print(event.toString());
+    if (event is BuildMapEvent) {
+      if (this.currentState is! InitialMapControlState) {
+        yield MapChangingState();
+      }
+      print('BuildMapEvent');
+      location = this.location;
+      mapType = event.mapType;
+
+      this.map = GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: this.location,
+          zoom: event.zoom,
+          tilt: event.tilt,
+        ),
+        mapType: this.mapType,
+        onMapCreated: (GoogleMapController newController) {
+          this.controller = newController;
+        },
+      );
+      yield MapReadyState();
+    }
+    if (event is CenterMapEvent) {
+      print('Centering');
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print('${position.latitude}, ${position.longitude}');
+      this.location = LatLng(position.latitude, position.longitude);
+      print(controller.getVisibleRegion());
+      controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: this.location,
+        zoom: this.zoom,
+        tilt: this.tilt,
+      )));
+      print('Animation finished');
+    }
+  }
+}
