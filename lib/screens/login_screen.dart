b0bf17c42aa68a4629c1802d1e60fd1457c7ba52
SimonSchwierzeck <simon.schwierzeck@stud.hs-hannover.de>
2019-10-10 12:38:34 +0200

    scrolling on login screen should now only be possibe on really low resolution devices and white bars on big resolution devices should be gone

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,7 +13,9 @@ class LoginScreen extends NesteoFullScreen {
   @override
   Widget build(BuildContext context) {
     final loc = Localizations.of<LocaleBase>(context, LocaleBase);
-    return SingleChildScrollView(
+    return Container(
+      constraints: BoxConstraints.expand(),
+      color: Colors.lightGreen,
       child: LoginScreenData(),
     );
   }
@@ -29,103 +31,105 @@ class LoginScreenData extends StatefulWidget {
 class _LoginScreenDataState extends State<LoginScreenData> {
   Widget build(BuildContext context) {
     final loc = Localizations.of<LocaleBase>(context, LocaleBase);
-    return Container(
-      color: Colors.lightGreen,
-      padding: EdgeInsets.only(bottom: 120),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 80),
-            child: Icon(
-              FontAwesomeIcons.kiwiBird,
-              size: 100,
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            'Nesteo',
-            style: TextStyle(
-              fontFamily: 'Quicksand',
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 100),
-            width: 300,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: loc.login.domain,
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.location_city, color: Colors.green),
+    return SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 80),
+                child: Icon(
+                  FontAwesomeIcons.kiwiBird,
+                  size: 100,
+                  color: Colors.white,
                 ),
               ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 15),
-            width: 300,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: loc.login.username,
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.person, color: Colors.green),
+              Text(
+                'Nesteo',
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 15),
-            width: 300,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: loc.login.password,
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.lock, color: Colors.green),
-                ),
-                obscureText: true,
-              ),
-            ),
-          ),
-          Container(
-            width: 250,
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: RaisedButton(
-                color: Colors.green,
-                onPressed: () {
-                  BlocProvider.of<PageControlBloc>(context)
-                      .dispatch(GoToMapEvent());
-                  BlocProvider.of<FrameControlBloc>(context)
-                      .dispatch(EnableFrameEvent());
-                },
-                child: Text(
-                  loc.login.logInButton,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+              Container(
+                padding: EdgeInsets.only(top: 100),
+                width: 300,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: loc.login.domain,
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon:
+                          Icon(Icons.location_city, color: Colors.green),
+                    ),
                   ),
                 ),
               ),
-            ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                width: 300,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: loc.login.username,
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(Icons.person, color: Colors.green),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                width: 300,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: loc.login.password,
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(Icons.lock, color: Colors.green),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              Container(
+                width: 250,
+                padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: RaisedButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      BlocProvider.of<PageControlBloc>(context)
+                          .dispatch(GoToMapEvent());
+                      BlocProvider.of<FrameControlBloc>(context)
+                          .dispatch(EnableFrameEvent());
+                    },
+                    child: Text(
+                      loc.login.logInButton,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(),
-        ],
+        ),
       ),
     );
   }
