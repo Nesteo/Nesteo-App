1fb3b32cd2a3f3b3e7cf506240670ef56750b756
SimonSchwierzeck <simon.schwierzeck@stud.hs-hannover.de>
2019-10-11 17:21:32 +0200

    added warning on back button when closing app #74

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -31,103 +31,122 @@ class LoginScreenData extends StatefulWidget {
 class _LoginScreenDataState extends State<LoginScreenData> {
   Widget build(BuildContext context) {
     final loc = Localizations.of<LocaleBase>(context, LocaleBase);
-    return SingleChildScrollView(
-      child: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 80),
-                child: Icon(
-                  FontAwesomeIcons.kiwiBird,
-                  size: 100,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'Nesteo',
-                style: TextStyle(
-                  fontFamily: 'Quicksand',
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 100),
-                width: 300,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: loc.login.domain,
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon:
-                          Icon(Icons.location_city, color: Colors.green),
-                    ),
+    return WillPopScope(
+      onWillPop: () => showDialog<bool>(
+        context: context,
+        builder: (c) => AlertDialog(
+          title: Text('Warning'),
+          content: Text('Do you really want to exit'),
+          actions: [
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () => Navigator.pop(c, true),
+            ),
+            FlatButton(
+              child: Text('No'),
+              onPressed: () => Navigator.pop(c, false),
+            ),
+          ],
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 80),
+                  child: Icon(
+                    FontAwesomeIcons.kiwiBird,
+                    size: 100,
+                    color: Colors.white,
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                width: 300,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: loc.login.username,
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: Icon(Icons.person, color: Colors.green),
-                    ),
+                Text(
+                  'Nesteo',
+                  style: TextStyle(
+                    fontFamily: 'Quicksand',
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                width: 300,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: loc.login.password,
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: Icon(Icons.lock, color: Colors.green),
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-              ),
-              Container(
-                width: 250,
-                padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: RaisedButton(
-                    color: Colors.green,
-                    onPressed: () {
-                      BlocProvider.of<PageControlBloc>(context)
-                          .dispatch(GoToMapEvent());
-                      BlocProvider.of<FrameControlBloc>(context)
-                          .dispatch(EnableFrameEvent());
-                    },
-                    child: Text(
-                      loc.login.logInButton,
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+                Container(
+                  padding: EdgeInsets.only(top: 100),
+                  width: 300,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: loc.login.domain,
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon:
+                            Icon(Icons.location_city, color: Colors.green),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  padding: EdgeInsets.only(top: 15),
+                  width: 300,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: loc.login.username,
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: Icon(Icons.person, color: Colors.green),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 15),
+                  width: 300,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: loc.login.password,
+                        border: InputBorder.none,
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: Icon(Icons.lock, color: Colors.green),
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 250,
+                  padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: RaisedButton(
+                      color: Colors.green,
+                      onPressed: () {
+                        BlocProvider.of<PageControlBloc>(context)
+                            .dispatch(GoToMapEvent());
+                        BlocProvider.of<FrameControlBloc>(context)
+                            .dispatch(EnableFrameEvent());
+                      },
+                      child: Text(
+                        loc.login.logInButton,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
