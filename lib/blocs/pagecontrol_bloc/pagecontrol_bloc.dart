44dd1c7d980754cee83ade935c81dc83186000c6
Priggelpitt <prgglptt@gmail.com>
2019-10-19 17:15:57 +0200

    Removed simulated loading times and transitions

--- lib/blocs/pagecontrol_bloc/pagecontrol_bloc.dart
+++ lib/blocs/pagecontrol_bloc/pagecontrol_bloc.dart
@@ -11,70 +11,27 @@ class PageControlBloc extends Bloc<PageControlEvent, PageControlState> {
     PageControlEvent event,
   ) async* {
     if (event is GoToMapEvent) {
-      yield ToMapScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
-      yield MapScreenState();
-    }
-
-    if (event is MapEvent) {
       yield MapScreenState();
     }
 
     if (event is GoToBoxListEvent) {
-      yield ToBoxListScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
-      yield BoxListScreenState();
-    }
-
-    if (event is BoxListEvent) {
       yield BoxListScreenState();
     }
 
     if (event is GoToNewBoxEvent) {
-      yield ToNewBoxScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
-      yield NewBoxScreenState();
-    }
-
-    if (event is NewBoxEvent) {
       yield NewBoxScreenState();
     }
 
     if (event is GoToBoxInfoEvent) {
-      yield ToBoxInfoScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
-      yield BoxInfoScreenState();
-    }
-
-    if (event is BoxInfoEvent) {
       yield BoxInfoScreenState();
     }
 
     if (event is GoToInspectionEvent) {
-      yield ToInspectionScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
-      yield InspectionScreenState();
-    }
-
-    if (event is InspectionEvent) {
       yield InspectionScreenState();
     }
 
     if (event is GoToNewInspectionEvent) {
-      yield ToNewInspectionScreenState();
-
-      await Future.delayed(Duration(seconds: 1));
       yield NewInspectionScreenState();
     }
-
-    if (event is NewInspectionEvent) {
-      yield NewInspectionScreenState();
-    }
-    // TODO: Add the missing events / states here
   }
 }
